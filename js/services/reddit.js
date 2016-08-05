31b0c6297ead54601cecf5dbe728e6992b01c01c
Evan McClaugherty <evan.mcclaugherty@gmail.com>
2016-08-05 16:18:56 -0600

    update service

--- js/services/reddit.js
+++ js/services/reddit.js
@@ -2,11 +2,19 @@ angular
     .module('wit')
     .factory('RedditService', RedditService);
 
-function RedditService($http) {
+function RedditService($http, GiphyService) {
+    var thoughts = [];
     return {
         getThoughts: function(query) {
             return $http.get(`https://www.reddit.com/r/Showerthoughts/search.json?q=${query}&restrict_sr=on&sort=relevance&t=all`).then(thoughts => {
-                return thoughts.data.data.children
+                var tempThoughts = thoughts.data.data.children;
+                tempThoughts.forEach(thought => {
+                    GiphyService.getGif(thought.data.title).then(image => {
+                        console.log(image);
+                        // thoughts.push(thought.data.title, image);
+                        // console.log(thoughts);
+                    })
+                })
             })
         }
     }
