b8c462f626e15c35d09f58a0a705ce3b64822ebc
Evan McClaugherty <evan.mcclaugherty@gmail.com>
2016-08-05 17:05:56 -0600

    fix img sources and combining giphy with title on one page, IT WORKS BITCH

--- js/services/reddit.js
+++ js/services/reddit.js
@@ -2,19 +2,11 @@ angular
     .module('wit')
     .factory('RedditService', RedditService);
 
-function RedditService($http, GiphyService) {
-    var thoughts = [];
+function RedditService($http) {
     return {
         getThoughts: function(query) {
             return $http.get(`https://www.reddit.com/r/Showerthoughts/search.json?q=${query}&restrict_sr=on&sort=relevance&t=all`).then(thoughts => {
-                var tempThoughts = thoughts.data.data.children;
-                tempThoughts.forEach(thought => {
-                    GiphyService.getGif(thought.data.title).then(image => {
-                        console.log(image);
-                        // thoughts.push(thought.data.title, image);
-                        // console.log(thoughts);
-                    })
-                })
+                return thoughts.data.data.children
             })
         }
     }
