025fec968f19dc9fffca2424c944dddb28d9f671
TheLucasMoore <lucasedwardmoore@gmail.com>
2016-08-05 15:21:06 -0600

    add then function to giphy service

--- js/services/giphy.js
+++ js/services/giphy.js
@@ -5,7 +5,7 @@ angular
   function GiphyService($http) {
 
   this.getGif = function(query) {
-    return $http.get("http://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=dc6zaTOxFJmzC")
+    return $http.get("http://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=dc6zaTOxFJmzC").then(giph => giph.data[0].url)
   };
 }
 GiphyService.$inject = ['$http']
