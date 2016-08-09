709b676842a35d3e08cc30547acefdf11e8228b5
TheLucasMoore <lucasedwardmoore@gmail.com>
2016-08-09 16:11:38 -0600

    Changed UI

--- public/js/controllers/home.js
+++ public/js/controllers/home.js
@@ -3,8 +3,8 @@ angular
     .controller('HomeController', HomeController);
 
 function HomeController($scope, $http, RedditService) {
-    $scope.redditThoughts = [];
     $scope.searchAPIs = function() {
+        $scope.redditThoughts = [];
         RedditService.getThoughts($scope.search).then(thoughts => {
             thoughts.forEach(ea => {
                 $http.get("https://api.giphy.com/v1/gifs/search?q=" + ea.data.title + "&api_key=dc6zaTOxFJmzC").then(giph => {
@@ -15,24 +15,15 @@ function HomeController($scope, $http, RedditService) {
                         image: giph.data.data[0].images.original.url,
                         imageURL: giph.data.data[0].url
                     });
+                    $scope.counter = 0;
+                    $scope.redditThought = $scope.redditThoughts[$scope.counter];
                 })
             })
         })
     }
-    $scope.change = function() {
-      $scope.redditThoughts = [];
-        RedditService.getThoughts($scope.search).then(thoughts => {
-            thoughts.forEach(ea => {
-                $http.get("https://api.giphy.com/v1/gifs/search?q=" + ea.data.title + "&api_key=dc6zaTOxFJmzC").then(giph => {
-                    $scope.redditThoughts.push({
-                        title: ea.data.title,
-                        author: ea.data.author,
-                        url: ea.data.url,
-                        image: giph.data.data[0].images.original.url,
-                        imageURL: giph.data.data[0].url
-                    });
-                })
-            })
-        })
+    $scope.nextThought = function() {
+      $scope.counter ++;
+      $scope.redditThought = $scope.redditThoughts[$scope.counter];
+      $scope.$apply();
     }
 }
