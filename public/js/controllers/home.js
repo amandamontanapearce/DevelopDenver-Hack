4d1bbe2fd268573fbccabd2c9a566a2ba65f653b
TheLucasMoore <lucasedwardmoore@gmail.com>
2016-08-09 00:23:47 -0600

    CSS fixes and search refresh

--- public/js/controllers/home.js
+++ public/js/controllers/home.js
@@ -2,7 +2,7 @@ angular
     .module('wit')
     .controller('HomeController', HomeController);
 
-function HomeController($scope, RedditService, $http) {
+function HomeController($scope, $http, RedditService) {
     $scope.redditThoughts = [];
     $scope.searchAPIs = function() {
         RedditService.getThoughts($scope.search).then(thoughts => {
@@ -10,7 +10,26 @@ function HomeController($scope, RedditService, $http) {
                 $http.get("http://api.giphy.com/v1/gifs/search?q=" + ea.data.title + "&api_key=dc6zaTOxFJmzC").then(giph => {
                     $scope.redditThoughts.push({
                         title: ea.data.title,
-                        image: giph.data.data[0].images.original.url
+                        author: ea.data.author,
+                        url: ea.data.url,
+                        image: giph.data.data[0].images.original.url,
+                        imageURL: giph.data.data[0].url
+                    });
+                })
+            })
+        })
+    }
+    $scope.change = function() {
+      $scope.redditThoughts = [];
+        RedditService.getThoughts($scope.search).then(thoughts => {
+            thoughts.forEach(ea => {
+                $http.get("http://api.giphy.com/v1/gifs/search?q=" + ea.data.title + "&api_key=dc6zaTOxFJmzC").then(giph => {
+                    $scope.redditThoughts.push({
+                        title: ea.data.title,
+                        author: ea.data.author,
+                        url: ea.data.url,
+                        image: giph.data.data[0].images.original.url,
+                        imageURL: giph.data.data[0].url
                     });
                 })
             })
