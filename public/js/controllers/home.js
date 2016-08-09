cdd6fbed83e47432b19eee00414e88627690b44f
TheLucasMoore <lucasedwardmoore@gmail.com>
2016-08-09 13:06:43 -0600

    https error on heroku

--- public/js/controllers/home.js
+++ public/js/controllers/home.js
@@ -7,7 +7,7 @@ function HomeController($scope, $http, RedditService) {
     $scope.searchAPIs = function() {
         RedditService.getThoughts($scope.search).then(thoughts => {
             thoughts.forEach(ea => {
-                $http.get("http://api.giphy.com/v1/gifs/search?q=" + ea.data.title + "&api_key=dc6zaTOxFJmzC").then(giph => {
+                $http.get("https://api.giphy.com/v1/gifs/search?q=" + ea.data.title + "&api_key=dc6zaTOxFJmzC").then(giph => {
                     $scope.redditThoughts.push({
                         title: ea.data.title,
                         author: ea.data.author,
@@ -23,7 +23,7 @@ function HomeController($scope, $http, RedditService) {
       $scope.redditThoughts = [];
         RedditService.getThoughts($scope.search).then(thoughts => {
             thoughts.forEach(ea => {
-                $http.get("http://api.giphy.com/v1/gifs/search?q=" + ea.data.title + "&api_key=dc6zaTOxFJmzC").then(giph => {
+                $http.get("https://api.giphy.com/v1/gifs/search?q=" + ea.data.title + "&api_key=dc6zaTOxFJmzC").then(giph => {
                     $scope.redditThoughts.push({
                         title: ea.data.title,
                         author: ea.data.author,
